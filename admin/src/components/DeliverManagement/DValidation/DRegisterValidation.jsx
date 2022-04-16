cf9705d3f8391f00420b7b0d7a5e875d0859750e
Sithum-Ravishka <79629419+Sithum-Ravishka@users.noreply.github.com>
2022-04-17 01:09:18 +0530

    Change DeliveryAssignList Page

---
+++ admin/src/components/DeliverManagement/DValidation/DRegisterValidation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function DRegisterValidation() {
+  return (
+    <div className="dAContainer">
+      <div className="dAHeader">
+        <Link to="/deliver-register" className="link">
+          <span className="dATitle">Order Assign Page</span>
+        </Link>
+      </div>
+    </div>
+  )
+}
--- api/routes/deliverassigns.js
+++ api/routes/deliverassigns.js
@@ -19,4 +19,35 @@ router.post("/", verify, async (req, res) => {
   }
 });
 
+//GET ALL
+
+router.get("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+      try {
+        const deliverassigns = await DeliverAssign.find();
+        res.status(200).json(deliverassigns.reverse());
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+    res.status(403).json("You are not allowed!");
+  }
+}); 
+
+//DELETE
+
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await DeliverAssign.findByIdAndDelete(req.params.id);
+      res.status(200).json("The movie has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+
 module.exports = router;
